43d19a3fa4fd64ffb1c66857e6f1a76eae44972a
raquelbrto <raquelbrito662@gmail.com>
2025-01-18 23:08:50 -0300

    chore resquest

--- src/main/java/com/imd/ecommerce/client/Store.java
+++ src/main/java/com/imd/ecommerce/client/Store.java
@@ -4,14 +4,15 @@ import com.imd.ecommerce.dto.ProductDTO;
 import com.imd.ecommerce.dto.TransactionDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "store", url = "${store.url}")
 public interface Store {
 
-    @GetMapping("/product")
-    ProductDTO getProduct(@RequestParam("product") long productId);
+    @GetMapping("/product/{productId}")
+    ProductDTO getProduct(@PathVariable("productId") long productId);
 
     @PostMapping("/sell")
     TransactionDTO createSell(@RequestParam("id") long id);
